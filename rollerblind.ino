d856226661a52e91f55e7e82071c868051aeeaaa
Eugene (RushFuture) Nepomniashchiy <rush.zlo@gmail.com>
2020-01-01 04:02:12 +0300

    feature: close tightly
    * Roller blind continue to roll about a second after crossing sensor.
    * Asterisk key disable both sensors, hash key enable that again.

--- rollerblind.ino
+++ rollerblind.ino
@@ -7,8 +7,8 @@ title Roll Blind
 state Idle
 state RollUp
 state RollDown
-state SlowUp
-state SlowDown
+state CloseUp
+state CloseDown
 
 [*] --> Idle
 
@@ -17,16 +17,16 @@ Idle -> RollDown : IRKeyDown
 
 RollUp -> Idle : IRKeyOK
 RollUp -> Idle : TimeoutUp
-RollUp --> SlowUp : SensorUp
+RollUp --> CloseUp : SensorUp
 
-SlowUp -> Idle : SlowTimeout
-SlowDown -> Idle : SlowTimeout
+CloseUp -> Idle : SlowTimeout
+CloseDown -> Idle : SlowTimeout
 
 note "All transitions to action state must add\na timed transition back to Idle state" as N1
 
 RollDown --> Idle : IRKeyOK
 RollDown --> Idle : TimeoutDown
-RollDown --> SlowDown : SensorDown
+RollDown --> CloseDown : SensorDown
 
 
 @enduml
@@ -37,31 +37,43 @@ RollDown --> SlowDown : SensorDown
 #include <IRremote.h>
 #include <L298N.h>
 
-#define SENSOR_TOP    11
-#define SENSOR_BOTTOM 10
+#define IR_PIN        13
 
-#define MOTOR_EN      3
-#define MOTOR_IN1     2
-#define MOTOR_IN2     4
+#define SENSOR_TOP    9
+#define SENSOR_BOTTOM 8
+#define SENSOR_ON     true
+#define SENSOR_OFF    false
 
-#define IR_KEY_UP     0xFF629D
-#define IR_KEY_DOWN   0xFFA857
+#define MOTOR_EN      10
+#define MOTOR_IN1     12
+#define MOTOR_IN2     11
+
+#define IR_KEY_UP         0xFF629D
+#define IR_KEY_DOWN       0xFFA857
+#define IR_KEY_ASTERIX    0xFF42BD
+#define IR_KEY_HASH       0xFF52AD
+#define IR_KEY_1          0xFF6897
+#define IR_KEY_3          0xFFB04F
 
 #define TIMEOUT_UP    32000
 #define TIMEOUT_DOWN  29000
+
+#define TIMEOUT_CLOSEDOWN  1000
+#define TIMEOUT_CLOSEUP    1000
 
 // FSM events
 #define EV_ROLL_UP          1
 #define EV_ROLL_DOWN        2
 #define EV_UNKNOWN_COMMAND	3
-#define EV_AT_TOP               4
-#define EV_AT_BOTTOM            5
+#define EV_AT_TOP           4
+#define EV_AT_BOTTOM        5
 
 
 class ObstacleSensor {
 public:
   ObstacleSensor(uint8_t _pin) {
     pin_ = _pin;
+    force_ = false;
   }
 
   // call bind() from setup()
@@ -72,12 +84,23 @@ public:
     ev_fall_ = _ev_cover;
   }
 
+  void enable() {
+    force_ = false;
+    update();
+  }
+
+  void disable(bool state) {
+    force_ = true;
+    state_ = state;
+  }
+
   bool blind() const {
     return state_;
   }
 
   // call update() in loop()
   void update() {
+    if (force_) return;
     bool newstate = (digitalRead(pin_) ==  LOW);
     if (newstate ^ state_) {
       state_ = newstate;
@@ -89,6 +112,7 @@ public:
 protected:
   uint8_t pin_;
   bool state_;
+  bool force_;
   Fsm* fsm_;
   int ev_raise_;
   int ev_fall_;
@@ -96,7 +120,7 @@ protected:
 
 L298N motor(MOTOR_EN, MOTOR_IN1, MOTOR_IN2);
 ObstacleSensor at_top(SENSOR_TOP), at_bottom(SENSOR_BOTTOM);
-IRrecv irrecv(12);
+IRrecv irrecv(IR_PIN);
 decode_results results;
 
 void on_idle_enter() {
@@ -124,9 +148,23 @@ void on_rolldown_enter() {
     motor.backward();
 }
 
+
+void on_slowup_enter() {
+    Serial.print("OK START ROLL-CLOSE-UP "); Serial.println(millis());
+}
+
+void on_slowdown_enter() {
+    Serial.print("OK START ROLL-CLOSE-DOWN "); Serial.println(millis());
+}
+
+
 State state_idle(&on_idle_enter, NULL, NULL);
 State state_rollup(&on_rollup_enter, NULL, NULL);
 State state_rolldown(&on_rolldown_enter, NULL, NULL);
+
+State state_slowup(&on_slowup_enter, NULL, NULL);
+State state_slowdown(&on_slowdown_enter, NULL, NULL);
+
 
 Fsm fsm_roblin(&state_idle);
 
@@ -157,6 +195,7 @@ void cmd_stop() {
 
 void setup() {
   Serial.begin(57600);
+  delay(1000);
   Serial.println("OK ROLLER-BLIND 1.0\n");
 
   motor.stop(); // stop motor
@@ -176,8 +215,11 @@ void setup() {
   fsm_roblin.add_transition(&state_rollup, &state_idle, EV_UNKNOWN_COMMAND, NULL);
   fsm_roblin.add_transition(&state_rolldown, &state_idle, EV_UNKNOWN_COMMAND, NULL);
 
-  fsm_roblin.add_transition(&state_rollup, &state_idle, EV_AT_TOP, NULL);
-  fsm_roblin.add_transition(&state_rolldown, &state_idle, EV_AT_BOTTOM, NULL);
+  fsm_roblin.add_transition(&state_rollup, &state_slowup, EV_AT_TOP, NULL);
+  fsm_roblin.add_transition(&state_rolldown, &state_slowdown, EV_AT_BOTTOM, NULL);
+
+  fsm_roblin.add_timed_transition(&state_slowup, &state_idle, TIMEOUT_CLOSEDOWN, NULL);
+  fsm_roblin.add_timed_transition(&state_slowdown, &state_idle, TIMEOUT_CLOSEUP, NULL);
 
 }
 
@@ -195,6 +237,20 @@ void loop() {
       cmd_rollup();
     } else if (results.value == IR_KEY_DOWN) {
       cmd_rolldown();
+    } else if (results.value == IR_KEY_ASTERIX) {
+      Serial.println( "MSG FORCE SENSORS OFF" );
+      at_top.disable(SENSOR_ON);
+      at_bottom.disable(SENSOR_OFF);
+    } else if (results.value == IR_KEY_HASH) {
+      Serial.println( "MSG FORCE SENSORS ON" );
+      at_top.enable();
+      at_bottom.enable();
+    } else if (results.value == IR_KEY_1) {
+      Serial.println( "MSG FAKE SENSOR TOP" );
+      fsm_roblin.trigger(EV_AT_TOP);
+    } else if (results.value == IR_KEY_3) {
+      Serial.println( "MSG FAKE SENSOR BOTTOM" );
+      fsm_roblin.trigger(EV_AT_BOTTOM);
     } else {
       cmd_stop();
     }
@@ -217,7 +273,7 @@ void loop() {
   at_bottom.update();
 
   fsm_roblin.run_machine();
-  delay(100);
+  delay(50);
   
 }
 
